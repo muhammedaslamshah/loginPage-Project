756b6871c38cd88572c71c745f73cd048e5d6e0a
Muhammed Aslam Shah S <119589957+muhammedaslamshah@users.noreply.github.com>
2023-07-28 13:08:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function submit(){
-let mail = document.getElementById('email').value;
-let pass = document.getElementById('password').value;
+function submitForm(event) {
+    event.preventDefault();
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
 
-const validemail="user@gmail.com";
-const validpassword="1234";
+    authenticateUser(email, password)
+        .then((isValid) => {
+            if (isValid) {
+                window.location.href = "welcomePage.html";
+            } else {
+                alert("Invalid email or password. Please try again later.");
+            }
+        })
+        .catch((error) => {
+            console.error("An error occurred during authentication:", error);
+            alert("An error occurred during login. Please try again later.");
+        });
+}
+function authenticateUser(email, password) {
+    return new Promise((resolve, reject) => {
+        const validEmail = "user@gmail.com";
+        const validPassword = "1234";
 
 
-
-    if( mail===validemail && pass===validpassword){
-        window.location.href="welcomePage.html"
-    }else{
-        alert("Invalid email or Password try again later.");
-    }
+        setTimeout(() => {
+            if (email === validEmail && password === validPassword) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        }, 1000);
+    });
 }
